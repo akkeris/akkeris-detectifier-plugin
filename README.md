7135e0235391bd0898a20cee79c32ddd8da62f56
Sam Beckett <sbeck14@me.com>
2021-02-23 13:56:25 -0700

    open source commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Akkeris Detectifier Plugin
+
+CLI commands for the Akkeris Detectifier.
+
+## Introduction
+
+The Akkeris Detectifier scans Akkeris app endpoints for security vulnerabilities with Detectify. It can run scans when a new Akkeris release is created on an app or on demand. 
+
+Scan results are available as a `security_scan` webhook, and for releases, through the Akkeris release status. Scan results are also available through a simple web interface - this includes report details and any associated errors.
+
+You can see a list of currently running scans using the CLI or through the web interface.
+
+For help with Detectify scores: https://blog.detectify.com/2017/05/24/interpret-detectify-score/
+
+## Requirements
+
+The environment variable `DETECTIFIER_URL` should be set to the endpoint of the plugin backend (see https://github.com/akkeris/akkeris-detectifier)
+
+## Commands
+
+`aka detectify:start -a APPNAME -t THRESHOLD -s SITE`
+
+Start a Detectify scan on a given application. 
+
+The "threshold" option is optional - it allows you to specify the maximum allowable threat score. Otherwise, the default is "6".
+
+The "site" option is optional - it acts as a temporary supercharged [site override](#site-overrides) that will override the default endpoint AND any configured site overrides for the app.
+
+`aka detectify:enable -a APPNAME`
+
+Enable Detectify scanning for a given application
+
+`aka detectify:disable -a APPNAME`
+
+Disable Detectify scanning for a given application
+
+`aka detectify:scans`
+
+Show currently running scans
+
+## Site Overrides
+
+Site overrides allow users to override the endpoint that is scanned for a given app. This is useful in various situations - for example, apps that have configured CSP filters won't allow any traffic on the default app endpoint, so they will need to scan the Akkeris site instead.
+
+Once a site override is set, any subsequent scan of the given app will use the site override URL instead of the default app endpoint.
+
+Please note that only a single site override may be created for an app. Multi-site scans for apps are not currently supported.
+
+### Commands
+
+`aka detectify:sites`
+
+Show all configured site overrides.
+
+`aka detectify:sites:set <SITE> -a APPNAME`
+
+Create a new site override for a given application
+
+`aka detectify:sites:unset -a APPNAME`
+
+Remove the site override for a given application
+
+## Future Plans
+
+- Show past scans using the CLI
+- PDF scan results (currently, Detectify does not offer this feature programmatically)
---
+++ index.js
@@ -0,0 +1,287 @@
+const open = require('open');
+
+const DETECTIFIER_URL = process.env.DETECTIFIER_URL;
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET || 'detectifer';
+
+function envCheck() {
+  if (!DETECTIFIER_URL || DETECTIFIER_URL === "") {
+    throw new Error("Missing DETECTIFIER_URL environment variable!");
+  }
+}
+
+async function start(appkit, args) {
+  try {
+    envCheck();
+  } catch (err) {
+    appkit.terminal.error(err);
+    return
+  }
+
+  const app = args.app;
+  const threshold = args.threshold;
+  let loading = appkit.terminal.loading('Submitting request');
+  try {
+    const response = await appkit.terminal.question(
+      appkit.terminal.markdown(`\n~~Are you sure you want to scan~~ ^^^${app}^^^ ~~?~~ (Y/N): `)
+    );
+    
+    if (!response || !(response.toLowerCase() === 'y' || response.toLowerCase() === 'yes')) {
+      console.log('Aborting...');
+      return;
+    } 
+    
+    const payload = {
+      "app_name": app,
+    };
+    if (threshold) {
+      payload.success_threshold = threshold;
+    }
+
+    loading.start();
+    const result = await appkit.api.post(JSON.stringify(payload), `${DETECTIFIER_URL}/v1/scans/start`);
+    loading.end();
+    if (result.message) {
+      console.log(result.message);
+    }
+  } catch (err) {
+    loading.end();
+    appkit.terminal.error(err);
+  }
+}
+
+async function enable(appkit, args) {
+  try {
+    envCheck();
+  } catch (err) {
+    appkit.terminal.error(err);
+    return
+  }
+
+  const { app, site } = args;
+  
+  try {
+    const hooks = await appkit.api.get(`/apps/${app}/hooks`);
+    const needsHook = !hooks.find(hook => hook.url === `${DETECTIFIER_URL}/v1/hook/released`);
+    if (needsHook) {
+      const hook = {
+        url: `${DETECTIFIER_URL}/v1/hook/released`,
+        active: true,
+        secret: WEBHOOK_SECRET,
+        events: ['released'],
+      };
+      await appkit.api.post(JSON.stringify(hook), `/apps/${app}/hooks`);
+      if (site && site !== "") {
+        await appkit.http.post(JSON.stringify({site}), `${DETECTIFIER_URL}/v1/config/${app}`, {'Content-type': 'application/json'});
+      }
+      console.log(appkit.terminal.markdown('^^Detectify scanning successfully enabled!^^'));
+    } else {
+      console.log(appkit.terminal.markdown('!!Detectify scanning is already enabled on this app!!'));
+    }
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+async function disable(appkit, args) {
+  try {
+    envCheck();
+  } catch (err) {
+    appkit.terminal.error(err);
+    return
+  }
+
+  const app = args.app;
+  try {
+    const hooks = await appkit.api.get(`/apps/${app}/hooks`);
+    const hook = hooks.find(hook => hook.url === `${DETECTIFIER_URL}/v1/hook/released`);
+    if (hook) {
+      await appkit.api.delete(`/apps/${app}/hooks/${hook.id}`);
+      console.log(appkit.terminal.markdown('^^Detectify scanning successfully disabled!^^'));
+    } else {
+      console.log(appkit.terminal.markdown('!!Detectify scanning is already disabled on this app!!'));
+    }
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+async function scans(appkit) {
+  try {
+    envCheck();
+  } catch (err) {
+    appkit.terminal.error(err);
+    return
+  }
+
+  try {
+    const scans = await appkit.api.get(`${DETECTIFIER_URL}/v1/scans`);
+    if (scans.length === 0) {
+      console.log(appkit.terminal.markdown('!! No currently running scans. !!'));
+    } else {
+      const formattedScans = scans.map(scan => ({
+        "App Name": scan.akkeris_app,
+        "Endpoint": scan.endpoint,
+        "Scan Status": scan.scan_status,
+        "Started At": (new Date(scan.created_at)).toLocaleString(),
+      }));
+      console.log(appkit.terminal.markdown('\n^^ Currently Running Scans ^^'));
+      appkit.terminal.table(formattedScans);
+    }
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+async function showUI(appkit) {
+  try {
+    envCheck();
+  } catch (err) {
+    appkit.terminal.error(err);
+    return
+  }
+  
+  try {
+    console.log('Opening Detectifier UI...');
+    await open(DETECTIFIER_URL);
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+async function listSites(appkit) {
+  try {
+    envCheck();
+  } catch (err) {
+    appkit.terminal.error(err);
+    return
+  }
+
+  try {
+    const config = await appkit.http.get(`${DETECTIFIER_URL}/v1/config`);
+    if (config.length === 0) {
+      console.log(appkit.terminal.markdown('\n!! No configured site overrides. !!'));
+    } else {
+      const formattedConfig = config.map(c => ({
+        "App Name": c.akkeris_app,
+        "Site": c.site,
+      }));
+      console.log(appkit.terminal.markdown('\n^^ Configured Site Overrides ^^'));
+      console.log(appkit.terminal.markdown('### The apps below will run scans on the associated site rather than the default app endpoint.###'))
+      appkit.terminal.table(formattedConfig);
+    }
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+async function setSite(appkit, args) {
+  try {
+    envCheck();
+  } catch (err) {
+    appkit.terminal.error(err);
+    return
+  }
+  
+  const { app, site } = args;
+  try {
+    await appkit.http.post(JSON.stringify({site}), `${DETECTIFIER_URL}/v1/config/${app}`, {'Content-type': 'application/json'});
+    console.log(appkit.terminal.markdown(`\n^^✓^^ ~~${site}~~ will now be targeted for any scan on the app ^^${app}^^`))
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+async function removeSite(appkit, args) {
+  try {
+    envCheck();
+  } catch (err) {
+    appkit.terminal.error(err);
+    return
+  }
+  
+  const { app } = args;
+  try {
+    await appkit.http.delete(`${DETECTIFIER_URL}/v1/config/${app}`);
+    console.log(appkit.terminal.markdown(`\n^^✓^^ Any scan on the app ^^${app}^^ will now use the default app endpoint.`))
+  } catch (err) {
+    appkit.terminal.error(err);
+  }
+}
+
+function init(appkit) {
+  const requireApp = {
+    app: {
+      alias: 'a',
+      string: true,
+      description: 'app name',
+      demand: true,
+    },
+  };
+
+  const startOptions = {
+    ...requireApp,
+    threshold: {
+      alias: 't',
+      string: true,
+      description: 'success threshold',
+      demand: false,
+    },
+    site: {
+      alias: 's',
+      string: true,
+      description: 'site to scan instead of app endpoint',
+      demand: false,
+    }
+  };
+
+  const enableOptions = {
+    ...requireApp,
+    site: {
+      alias: 's',
+      string: true,
+      description: 'add site override that will be scanned instead of app endpoint',
+      demand: false,
+    }
+  }
+
+  appkit.args
+    .command('detectify:start', 'Start an immediate detectify scan on an app', startOptions, start.bind(null, appkit))
+    .command('detectify:run', false, startOptions, start.bind(null,appkit))
+
+    .command('detectify:enable', 'Enable detectify commands on an app', enableOptions, enable.bind(null, appkit))
+    .command('detectify:add', false, enableOptions, enable.bind(null, appkit))
+    
+    .command('detectify:disable', 'Disable detectify scans on an app', requireApp, disable.bind(null, appkit))
+    .command('detectify:remove', false, requireApp, disable.bind(null, appkit))
+
+    .command('detectify:sites', 'View list of configured site overrides', {}, listSites.bind(null, appkit))
+    .command('detectify:overrides', false, {}, listSites.bind(null, appkit))
+
+    .command('detectify:sites:set <SITE>', 'Set a site override for an app', requireApp, setSite.bind(null, appkit))
+    .command('detectify:sites:add <SITE>', false, requireApp, setSite.bind(null, appkit))
+    .command('detectify:overrides:set <SITE>', false, requireApp, setSite.bind(null, appkit))
+    .command('detectify:overrides:add <SITE>', false, requireApp, setSite.bind(null, appkit))
+
+    .command('detectify:sites:unset', 'Remove a site override for an app', requireApp, removeSite.bind(null, appkit))
+    .command('detectify:sites:remove', false, requireApp, removeSite.bind(null, appkit))
+    .command('detectify:sites:destroy', false, requireApp, removeSite.bind(null, appkit))
+    .command('detectify:sites:delete', false, requireApp, removeSite.bind(null, appkit))
+    .command('detectify:overrides:unset', false, requireApp, removeSite.bind(null, appkit))
+    .command('detectify:overrides:remove', false, requireApp, removeSite.bind(null, appkit))
+    .command('detectify:overrides:destroy', false, requireApp, removeSite.bind(null, appkit))
+    .command('detectify:overrides:delete', false, requireApp, removeSite.bind(null, appkit))
+    
+    .command('detectify:scans', 'Show currently running Detectify scans', {}, scans.bind(null, appkit))
+    .command('detectify:running', false, {}, scans.bind(null, appkit))
+    .command('detectify:current', false, {}, scans.bind(null, appkit))
+    
+    .command('detectify:ui', 'Open the Detectifier UI in a web browser', {}, showUI.bind(null, appkit));
+}
+
+module.exports = {
+  init,
+  update: () => {},
+  group: 'detectify',
+  help: 'Manage Detectify scans for applications',
+  primary: true,
+};
